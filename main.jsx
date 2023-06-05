9b3626ee6f14a7e46dc2a4bc5ad0d767fa996ff3
Johana Madero <johanamadero3@gmail.com>
2023-06-05 01:22:04 -0300

    Principal section

--- main.jsx
+++ main.jsx
@@ -1,13 +1,16 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './src/components/navBar/navBar';
+import FrontPage from './src/pages/frontPage/frontPage';
 import './style.css';
 
 createRoot(document.getElementById('app')).render(
 	<div>
 		<BrowserRouter>
 			<NavBar />
-			<Routes>{/* <Route path="/" element="" /> */}</Routes>
+			<Routes>
+				<Route path="/" element={<FrontPage />} />
+			</Routes>
 		</BrowserRouter>
 	</div>
 );
---
+++ src/pages/frontPage/frontPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './frontPage.css';
+
+function FrontPage() {
+	return (
+		<>
+			<div className="titleContainer">
+				<div>
+					<h1>THE POWER GYM</h1>
+					<p className="legend">Your neighborhood gym</p>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default FrontPage;
